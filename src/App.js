f907950ab2cb7ad7bc504fc160b8b0ff7301eaa8
Kadyr Poyraz <poyraz_2001@mail.ru>
2019-12-11 10:24:40 +0200

    Создал запрос всех стран в componentDidMount, используя метод countryRequest

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React, {Component} from 'react';
+import axios from 'axios';
 
 class App extends Component {
     state = {
         countries: []
+    };
+    async componentDidMount() {
+        await this.countryRequest();
+    }
+    countryRequest = async () => {
+        const resp = await axios.get('https://restcountries.eu/rest/v2/all?fields=name;alpha3Code');
+        this.setState({countries: resp.data})
     };
     render() {
         return (
